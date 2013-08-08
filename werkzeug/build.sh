b6c26a81cfcc4df84e0f0cd8712f9557d71e9a66
Bojan Delic <bojan@delic.in.rs>
2013-08-08 09:07:05 +0200

    Added werkzeug

---
+++ werkzeug/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$PYTHON setup.py install_lib
+$PYTHON setup.py install_data
+
+# Add more build steps here, if they are necessary.
+
+# See
+# https://github.com/ContinuumIO/conda/blob/master/conda/builder/README.txt
+# for a list of environment variables that are set during the build process.
